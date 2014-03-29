39dabd3e73e278f3bc6368ad35ed009e66df165c
deryademirer <dry_1400@hotmail.com>
2014-03-29 22:53:33 +0200

    Update Learning.java

--- JavaProgramming/src/lib/cloudist/Learning.java
+++ JavaProgramming/src/lib/cloudist/Learning.java
@@ -1,10 +1,28 @@
-package lib.cloudist;
-
+package cloudist;
+import java.util.Scanner;
 public class Learning {
 
-	
-	public void Print() 
+	public void Print()
 	{
 		System.out.println("Hello world");
 	}
+	
+	
+	//cümledeki kelime sayisi
+	public static String kelimeSay(String a)
+	{
+		Scanner input=new Scanner(System.in);
+		int b=0;
+		for(int i=0;i<a.length();i++)
+		{
+			if(a.charAt(i)==' ' && a.charAt(i+1)!=' ' )
+			{
+				b=b+1;
+			}
+		}
+		b=b+1; //cümledeki son kelimeyi burada ekliyoruz.
+		String sayac=Integer.toString(b);
+		return sayac;
+	}
+
 }
