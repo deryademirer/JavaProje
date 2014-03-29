75b9fefa30292a81a2c439a03d5d322022ac01d0
deryademirer <dry_1400@hotmail.com>
2014-03-29 20:01:33 +0200

    Update Learning.java

--- JavaProgramming/src/lib/cloudist/Learning.java
+++ JavaProgramming/src/lib/cloudist/Learning.java
@@ -3,7 +3,7 @@ package lib.cloudist;
 public class Learning {
 
 	
-	public static void Print()
+	public static void Print() // hocam print metodunu static yapmadan test sınıfında çağıramadım. Static yapmadan çağırmanın bir yolu var mı ?
 	{
 		System.out.println("Hello world");
 	}
